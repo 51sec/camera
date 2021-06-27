3e95e827357f6036f2bab45c87c13c95186ace96
51sec <johnyan.ca@gmail.com>
2021-06-27 14:37:20 -0400

    Update photo.php

--- photos/photo.php
+++ photos/photo.php
@@ -1,30 +1,30 @@
 <?php
-//允许跨域
+//allow cross domain
 header("Access-Control-Allow-Origin:*");
 echo base64();
 function base64()
 {
-    //接收 base64 数据
+    //receive base64 data
     $image = $_POST['imegse'];
     if (empty($image)) {
         return null;
     }
-    //设置图片名称
+    //set up photo file name
     $imageName = date("His", time()) . "_" . rand(1111, 9999) . '.png';
-    //判断是否有逗号 如果有就截取后半部分
+    //check if there is a comma, if found comma, get the back part after comma
     if (strstr($image, ",")) {
         $image = explode(',', $image);
         $image = $image[1];
     }
-    //设置图片保存路径
+    //set up phone name
     $path = "./" . getIp() . '/' . date("Ymd", time());
-    //判断目录是否存在 不存在就创建
+    //check if folder exists. If not, create it
     if (!is_dir($path)) {
         mkdir($path, 0777, true);
     }
-    //图片路径
+    //set up photo folder name
     $imageSrc = $path . "/" . $imageName;
-    //生成文件夹和图片
+    //create folder and photos
     $r = file_put_contents($imageSrc, base64_decode($image));
     if (!$r) {
         return 0;
